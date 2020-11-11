0019250d58cf156d35cf4844810ce1935a3cad09
Sunny.K <sunnyk5780@gmail.com>
2020-11-11 19:45:45 +0900

    function style의 life cycle

--- src/FuncComp.js
+++ src/FuncComp.js
@@ -6,18 +6,36 @@ export default function FuncComp(props) {
   const [number, setRandomNumber] = useState(props.initNumber)
   const [_date, setDate] = useState((new Date()).toString())
 
+  useEffect(function () {
+    console.log('%cfunction => useEffect (componentDidMount)', logTextColor)
+    document.title = number
+
+    return function () {
+      console.log('%cfunction => useEffect return (componentDidMount)', logTextColor)
+    }
+  }, [number])
+
   // 컴포넌트가 생성, 업데이트된 직후 호출
   // side effect(부가적인 작용)
   useEffect(function () {
-    console.log('%cfunction => useEffect(componentDidMount & componentDidUpdate) ', logTextColor)
-    document.title = `${number} : ${_date}`
+    console.log('%cfunction => useEffect _date (componentDidMount & componentDidUpdate)', logTextColor)
+    document.title = _date
 
-    // 기존 컴포넌트를 지우고 새로 그리기 전에 호출
+    // 기존 컴포넌트를 지우고 (새로 그리기 전에) 호출
     // cleanup
     return function () {
-      console.log('%cfunction => useEffect return', logTextColor)
+      console.log('%cfunction => useEffect _date return', logTextColor)
     }
-  })
+  }, [_date])
+
+  useEffect(function () {
+    console.log('%cfunction => useEffect number (componentDidMount & componentDidUpdate)', logTextColor)
+    document.title = number
+
+    return function () {
+      console.log('%cfunction => useEffect number return', logTextColor)
+    }
+  }, [number])
 
   console.log('%cfunction => render', logTextColor)
   return (
