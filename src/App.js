b2109e50b9af0bb8a3c89cf1530439d9810f01ab
Sunny.K <sunnyk5780@gmail.com>
2020-11-11 14:58:26 +0900

    수업의 목표 및 실습환경 - 간단한 function, class 형 컴포넌트 작성 - css 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import React from 'react'
+import FuncComp from './FuncComp.js'
+import ClassComp from './ClassComp.js'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      Hello world!
+    <div className="container">
+      <h1>Hello world!</h1>
+      <FuncComp></FuncComp>
+      <ClassComp></ClassComp>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/ClassComp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class ClassComp extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>Class Style Component</h2>
+      </div>
+    )
+  }
+}
