ee8ae8e70f53102116d0f1c5f97ace053a54b5ec
Sunny.K <sunnyk5780@gmail.com>
2020-11-11 18:28:01 +0900

    function style의 useEffect()

--- src/FuncComp.js
+++ src/FuncComp.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+const logTextColor = 'color: blue'
 
 export default function FuncComp(props) {
   const [number, setRandomNumber] = useState(props.initNumber)
   const [_date, setDate] = useState((new Date()).toString())
 
+  // 컴포넌트가 생성, 업데이트된 직후 호출
+  // side effect(부가적인 작용)
+  useEffect(function () {
+    console.log('%cfunction => useEffect(componentDidMount & componentDidUpdate) ', logTextColor)
+    document.title = `${number} : ${_date}`
+
+    // 기존 컴포넌트를 지우고 새로 그리기 전에 호출
+    // cleanup
+    return function () {
+      console.log('%cfunction => useEffect return', logTextColor)
+    }
+  })
+
+  console.log('%cfunction => render', logTextColor)
   return (
     <div className="container">
       <h2>Function Style Component</h2>
