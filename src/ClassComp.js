16a43506182d54d8821f4fd458e02cf6e14dd393
Sunny.K <sunnyk5780@gmail.com>
2020-11-11 15:50:48 +0900

    function style에서 useState 사용하기

--- src/ClassComp.js
+++ src/ClassComp.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default class ClassComp extends React.Component {
   state = {
-    number: this.props.initNumber
+    number: this.props.initNumber,
+    date: (new Date()).toString()
   }
 
   render() {
@@ -10,9 +11,15 @@ export default class ClassComp extends React.Component {
       <div className="container">
         <h2>Class Style Component</h2>
         <p>Number : {this.state.number}</p>
+        <p>Now : {this.state.date}</p>
         <input type="button" value="random" onClick={
           function () {
             this.setState({ number: Math.random() })
+          }.bind(this)
+        } />
+        <input type="button" value="now" onClick={
+          function () {
+            this.setState({ date: (new Date()).toString() })
           }.bind(this)
         } />
       </div>
--- src/FuncComp.js
+++ src/FuncComp.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function FuncComp(props) {
+  const [number, setRandomNumber] = useState(props.initNumber)
+  const [_date, setDate] = useState((new Date()).toString())
+
   return (
     <div className="container">
       <h2>Function Style Component</h2>
-      <p>Number : {props.initNumber}</p>
+      <p>Number : {number}</p>
+      <p>Date : {_date}</p>
+      <input type="button" value="random" onClick={
+        function () {
+          setRandomNumber(Math.random())
+        }
+      } />
+      <input type="button" value="date" onClick={
+        function () {
+          setDate((new Date()).toString())
+        }
+      } />
     </div>
   )
 }
