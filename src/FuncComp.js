2d7dabb553c263488f56711c546e86b47ee14a80
sunyoung <sunnyk5780@gmail.com>
2020-11-15 15:33:42 +0900

    function 스타일에서 componentDidMount와 componentWillUnMount 구현

--- src/FuncComp.js
+++ src/FuncComp.js
@@ -1,58 +1,70 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react"
 
-const logTextColor = 'color: blue'
+const logTextColor = "color: skyblue"
 
 export default function FuncComp(props) {
   const [number, setRandomNumber] = useState(props.initNumber)
-  const [_date, setDate] = useState((new Date()).toString())
+  const [_date, setDate] = useState(new Date().toString())
 
-  useEffect(function () {
-    console.log('%cfunction => useEffect (componentDidMount)', logTextColor)
-    document.title = number
+  useEffect(
+    function () {
+      console.log("%cfunction => useEffect (componentDidMount)", logTextColor)
+      document.title = number
 
-    return function () {
-      console.log('%cfunction => useEffect return (componentDidMount)', logTextColor)
-    }
-  }, [number])
+      return function () {
+        console.log("%cfunction => useEffect return (componentDidMount)", logTextColor)
+      }
+    },
+    [number]
+  )
 
   // 컴포넌트가 생성, 업데이트된 직후 호출
   // side effect(부가적인 작용)
+  useEffect(
+    function () {
+      console.log("%cfunction => useEffect _date (componentDidMount & componentDidUpdate)", logTextColor)
+      document.title = _date
+
+      // 기존 컴포넌트를 지우고 (새로 그리기 전에) 호출
+      // cleanup
+      return function () {
+        console.log("%cfunction => useEffect _date return (componentDidMount & componentDidUpdate)", logTextColor)
+      }
+    },
+    [_date]
+  )
+
+  // 두 번째 배열안의 인자들의 상태가 바뀌어야만 첫 번째 인자인 콜백함수 호출
+  // 빈 배열 전달 시 최초 1회만 실행되고 그 이후에는 실행되지 않음
   useEffect(function () {
-    console.log('%cfunction => useEffect _date (componentDidMount & componentDidUpdate)', logTextColor)
-    document.title = _date
-
-    // 기존 컴포넌트를 지우고 (새로 그리기 전에) 호출
-    // cleanup
-    return function () {
-      console.log('%cfunction => useEffect _date return', logTextColor)
-    }
-  }, [_date])
-
-  useEffect(function () {
-    console.log('%cfunction => useEffect number (componentDidMount & componentDidUpdate)', logTextColor)
-    document.title = number
+    console.log("%cfunction => useEffect (componentDidMount)", logTextColor)
 
     return function () {
-      console.log('%cfunction => useEffect number return', logTextColor)
+      // 컴포넌트가 삭제될 때 실행
+      console.log("%cfunction => useEffect return (componentWillUnMount)", logTextColor)
     }
-  }, [number])
+  }, [])
 
-  console.log('%cfunction => render', logTextColor)
+  console.log("%cfunction => render", logTextColor)
   return (
     <div className="container">
       <h2>Function Style Component</h2>
       <p>Number : {number}</p>
       <p>Date : {_date}</p>
-      <input type="button" value="random" onClick={
-        function () {
+      <input
+        type="button"
+        value="random"
+        onClick={function () {
           setRandomNumber(Math.random())
-        }
-      } />
-      <input type="button" value="date" onClick={
-        function () {
-          setDate((new Date()).toString())
-        }
-      } />
+        }}
+      />
+      <input
+        type="button"
+        value="date"
+        onClick={function () {
+          setDate(new Date().toString())
+        }}
+      />
     </div>
   )
 }
